7689e82409afb091473163304c41b6678a2723af
Alex Tennant <adtennant@gmail.com>
2015-09-09 12:12:00 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # pebblejs-typescript-definitions
-Provides TypeScript definitions for the PebbleJS library.
+Provides TypeScript definitions and constant values for [Pebble.js](https://github.com/pebble/pebblejs) to allow you to write strongly typed Pebble apps using TypeScript.
+
+## Usage
+This repository consists of two files:
+* pebblejs.d.ts - Provides TypeScript definitions for the documented parts of Pebble.js.
+* pebble.consts.ts - Provides a number of `const` values which can be used to replace string values passed to Pebble.js functions (e.g. `COLOR_BLACK` instead of `'black'`, or `VIBE_SHORT` instead of `'short'`). This file is optional.
+
+Include pebblejs.d.ts (and pebble.consts.ts if you want to use it) in the `/src/js` folder of your Pebble.js project, then make sure you compile your TypeScript files with `--module 'commonjs'` before building your project.
+
+## Example
+
+```
+import UI = require('ui');
+import Consts = require('pebblejs.consts');
+
+var card = new UI.Card({
+    title: 'Hello World',
+    body: 'This is your first Pebble app!',
+    scrollable: true,
+    backgroundColor: Consts.COLOR_PICTON_BLUE
+});
+
+card.on(Consts.WINDOW_CLICK, function(e) {
+    card.subtitle('Button ' + e.button + ' clicked!');
+});
+
+card.show();
+```
---
+++ pebblejs.consts.ts
@@ -0,0 +1,126 @@
+module Consts {
+    export const WEEKDAY_SUNDAY : string = 'sunday';
+    export const WEEKDAY_MONDAY : string = 'monday';
+    export const WEEKDAY_TUESDAY : string = 'tuesday';
+    export const WEEKDAY_WEDNESDAY : string = 'wednesday';
+    export const WEEKDAY_THURSDAY : string = 'thursday';
+    export const WEEKDAY_FRIDAY : string = 'friday';
+    export const WEEKDAY_SATURDAY : string = 'saturday';
+
+    export const COLOR_CLEAR : string = 'clear';
+    export const COLOR_BLACK : string = 'black';
+    export const COLOR_WHITE : string = 'white';
+
+    export const COLOR_ARMY_GREEN : string = 'armyGreen';
+    export const COLOR_BABY_BLUE_EYES : string = 'babyBlueEyes';
+    export const COLOR_BLUE : string = 'blue';
+    export const COLOR_BLUE_MOON : string = 'blueMoon';
+    export const COLOR_BRASS : string = 'brass';
+    export const COLOR_BRIGHT_GREEN : string = 'brightGreen';
+    export const COLOR_BRILLIANT_ROSE : string = 'brilliantRose';
+    export const COLOR_BULGARIAN_ROSE : string = 'bulgarianRose';
+    export const COLOR_CADET_BLUE : string = 'cadetBlue';
+    export const COLOR_CELESTE : string = 'celeste';
+    export const COLOR_CHROME_YELLOW : string = 'chromeYellow';
+    export const COLOR_COBALT_BLUE : string = 'cobaltBlue';
+    export const COLOR_CYAN : string = 'cyan';
+    export const COLOR_DARK_CANDY_APPLE_RED : string = 'darkCandyAppleRed';
+    export const COLOR_DARK_GRAY : string = 'darkGray';
+    export const COLOR_DARK_GREEN : string = 'darkGreen';
+    export const COLOR_DUKE_BLUE : string = 'dukeBlue';
+    export const COLOR_ELECTRIC_BLUE : string = 'electricBlue';
+    export const COLOR_ELECTRIC_ULTRAMARINE : string = 'electricUltramarine';
+    export const COLOR_FASHION_MAGENTA : string = 'fashionMagenta';
+    export const COLOR_FOLLY : string = 'folly';
+    export const COLOR_GREEN : string = 'green';
+    export const COLOR_ICTERINE : string = 'icterine';
+    export const COLOR_IMPERIAL_PURPLE : string = 'imperialPurple';
+    export const COLOR_INCHWORM : string = 'inchworm';
+    export const COLOR_INDIGO : string = 'indigo';
+    export const COLOR_ISLAMIC_GREEN : string = 'islamicGreen';
+    export const COLOR_JAEGER_GREEN : string = 'jaegerGreen';
+    export const COLOR_JAZZBERRY_JAM : string = 'jazzberryJam';
+    export const COLOR_KELLY_GREEN : string = 'kellyGreen';
+    export const COLOR_LAVENDER_INDIGO : string = 'lavenderIndigo';
+    export const COLOR_LIBERTY : string = 'liberty';
+    export const COLOR_LIGHT_GRAY : string = 'lightGray';
+    export const COLOR_LIMERICK : string = 'limerick';
+    export const COLOR_MAGENTA : string = 'magenta';
+    export const COLOR_MALACHITE : string = 'malachite';
+    export const COLOR_MAY_GREEN : string = 'mayGreen';
+    export const COLOR_MEDIUM_AQUAMARINE : string = 'mediumAquamarine';
+    export const COLOR_MEDIUM_SPRING_GREEN : string = 'mediumSpringGreen';
+    export const COLOR_MELON : string = 'melon';
+    export const COLOR_MIDNIGHT_GREEN : string = 'midnightGreen';
+    export const COLOR_MINT_GREEN : string = 'mintGreen';
+    export const COLOR_ORANGE : string = 'orange';
+    export const COLOR_OXFORD_BLUE : string = 'oxfordBlue';
+    export const COLOR_PASTEL_YELLOW : string = 'pastelYellow';
+    export const COLOR_PICTON_BLUE : string = 'pictonBlue';
+    export const COLOR_PURPLE : string = 'purple';
+    export const COLOR_PURPUREUS : string = 'purpureus';
+    export const COLOR_RAJAH : string = 'rajah';
+    export const COLOR_RED : string = 'red';
+    export const COLOR_RICH_BRILLIANT_LAVENDER : string = 'richBrilliantLavender';
+    export const COLOR_ROSE_VALE : string = 'roseVale';
+    export const COLOR_SCREAMIN_GREEN : string = 'screamingGreen';
+    export const COLOR_SHOCKING_PINK : string = 'shockingPink';
+    export const COLOR_SPRING_BUD : string = 'springBud';
+    export const COLOR_SUNSET_ORANGE : string = 'sunsetOrange';
+    export const COLOR_TIFFANY_BLUE : string = 'tiffanyBlue';
+    export const COLOR_VERY_LIGHT_BLUE : string = 'veryLightBlue';
+    export const COLOR_VIVID_CERULEAN : string = 'vividCerulean';
+    export const COLOR_VIVID_VIOLET : string = 'vividViolet';
+    export const COLOR_WINDSOR_TAN : string = 'windsorTan';
+    export const COLOR_YELLOW : string = 'yellow';
+
+    export const BUTTON_UP : string = 'up';
+    export const BUTTON_SELECT : string = 'select';
+    export const BUTTON_DOWN : string = 'down';
+    export const BUTTON_BACK : string = 'back';
+
+    export const ACCEL_TAP : string = 'tap';
+    export const ACCEL_DATA : string = 'data';
+
+    export const WINDOW_ACCELDATA : string = 'accelData';
+    export const WINDOW_SHOW : string = 'show';
+    export const WINDOW_HIDE : string = 'hide';
+
+    export const WINDOW_CLICK : string = 'click';
+    export const WINDOW_LONG_CLICK : string = 'longClick';
+
+    export const CARD_STYLE_SMALL : string = 'small';
+    export const CARD_STYLE_LARGE : string = 'large';
+    export const CARD_STYLE_MONO : string = 'mono';
+
+    export const MENU_SELECT : string = 'select';
+    export const MENU_LONG_SELECT : string = 'longSelect';
+
+    export const TEXT_OVERFLOW_WRAP : string = 'wrap';
+    export const TEXT_OVERFLOW_ELLIPSIS : string = 'ellipsis';
+    export const TEXT_OVERFLOW_FILL : string = 'fill';
+
+    export const TEXT_ALIGN_LEFT : string = 'left';
+    export const TEXT_ALIGN_CENTER : string = 'center';
+    export const TEXT_ALIGN_RIGHT : string = 'right';
+
+    export const TIME_UNITS_YEARS : string = 'years';
+    export const TIME_UNITS_MONTHS : string = 'months';
+    export const TIME_UNITS_DAYS : string = 'days';
+    export const TIME_UNITS_HOURS : string = 'hours';
+    export const TIME_UNITS_MINUTES : string = 'minutes';
+    export const TIME_UNITS_SECONDS : string = 'seconds';
+
+    export const COMPOP_NORMAL : string = 'normal';
+    export const COMPOP_INVERT : string = 'invert';
+    export const COMPOP_OR : string = 'or';
+    export const COMPOP_AND : string = 'and';
+    export const COMPOP_CLEAR : string = 'clear';
+    export const COMPOP_SET : string = 'set';
+
+    export const VIBE_SHORT : string = 'short';
+    export const VIBE_LONG : string = 'long';
+    export const VIBE_DOUBLE : string = 'double';
+}
+
+export = Consts;
